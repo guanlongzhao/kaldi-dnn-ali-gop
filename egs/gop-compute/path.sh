d721946d045d6c438479ede8119a2a7845aeb722
Guanlong Zhao <guanlongzhao@gmail.com>
2018-09-20 15:50:02 -0500

    change dependencies

--- egs/gop-compute/path.sh
+++ egs/gop-compute/path.sh
@@ -1,6 +1,6 @@
-export KALDI_ROOT=/home/ming/Documents/software/kaldi # Change to your own KALDI_ROOT
+export KALDI_ROOT=/home/guanlong/Tools/kaldi # Change to your own KALDI_ROOT
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/gopbin:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
